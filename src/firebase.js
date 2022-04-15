359fbfe8baad875e2ac3a74b016f3e65dd9bbb73
clementdufour94 <clement.dufourl@gmail.com>
2022-04-15 15:35:32 +0200

    CreateHeader

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC6obk7-kapjNeZry8tSqNvuPAd8897JtY",
+    authDomain: "clone-ff504.firebaseapp.com",
+    projectId: "clone-ff504",
+    storageBucket: "clone-ff504.appspot.com",
+    messagingSenderId: "79273600426",
+    appId: "1:79273600426:web:9a74f6255f9a1e955237d3",
+    measurementId: "G-VRY096VL54"
+  };

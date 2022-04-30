f861c0a4bc4c974be5afe6135d4654ebfd0d91f4
clementdufour94 <clement.dufourl@gmail.com>
2022-04-30 16:54:07 +0200

    addproduct

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react'
+import "./AddProduct.css"
 
 function AddProduct() {
     const [postId, setPostId] = useState(null);
@@ -17,7 +18,77 @@ function AddProduct() {
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
     }, []);
   return (
-    <div>AddProduct</div>
+    <div className="payment">
+        <div className="payment__container">
+            <h1> Add Product</h1>
+            <div className="payment__section">
+                <div className="payment__title">
+                    <h3>Title</h3>
+
+                </div>
+                <div className="payment__address">
+                  
+                    <input type="text" placeholder="Enter name of product"></input>
+
+                </div>
+
+            </div>
+            <div className="payment__section">
+                <div className="payment__title">
+                        <h3>Description</h3>
+
+                </div>
+                <div className="payment__items">
+                    <textarea cols="100" rows="3"></textarea>
+                </div>
+
+            </div>
+            <div className="payment__section">
+                <div className="payment__title">
+                        <h3>Photo</h3>
+                </div>
+                <div className="payment__details">
+                    <form>
+                        
+                        <div className="payment__priceContainer">
+                        
+                        <input type="file"></input>
+
+                        </div>
+                        
+                    </form>
+
+                </div>
+
+            </div>
+            <div className="payment__section">
+                <div className="payment__title">
+                        <h3>Price</h3>
+                </div>
+                <div className="payment__details">
+                    <form>
+                        
+                        <div className="payment__priceContainer">
+                        
+                        <input type="float" placeholder="Enter price"></input>
+
+                        </div>
+                        
+                    </form>
+
+                </div>
+
+            </div>
+            
+
+        </div>
+        
+          <button className="addproduct-btn">Add Product</button>
+
+      
+        
+
+    </div>
   )
 }
 
